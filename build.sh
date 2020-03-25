0ffd61f54ad8df7045467a3ee65af5fe7ad94e1e
Shahnawaz Alam <shahnawaz.alam.dev@gmail.com>
2020-03-25 18:22:27 +0100

    Incorporate lib build into CI (#32)

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Updating libboost to run the cmake (requirement) 
+sudo apt-get install libboost-all-dev
+# Updating libprocps to run the cmake (requirement) 
+sudo apt-get install libprocps-dev
+
+rm -r libsnark
+git submodule init
+git submodule update --init --recursive
+
 cd libsnark
 mkdir build
 cd build
-git submodule update --init --recursive
 
 cmake ..
 
