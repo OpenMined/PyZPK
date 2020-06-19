d92d543da8f8bdf1404c5f32c6979a7a2a27df9a
gargarchit <garg4.ag@gmail.com>
2020-06-19 18:12:06 +0530

    Add Binding for uscs_to_qap reduction

--- src/PyZPK/binding.cpp
+++ src/PyZPK/binding.cpp
@@ -16,6 +16,7 @@ void init_relations_arithmetic_programs_qap(py::module &);
 void init_relations_arithmetic_programs_sap(py::module &);
 void init_relations_arithmetic_programs_ssp(py::module &);
 void init_reductions_r1cs_to_qap(py::module &);
+void init_reductions_uscs_to_ssp(py::module &);
 
 PYBIND11_MODULE(pyzpk, m)
 {
@@ -36,4 +37,5 @@ PYBIND11_MODULE(pyzpk, m)
     init_relations_arithmetic_programs_sap(m);
     init_relations_arithmetic_programs_ssp(m);
     init_reductions_r1cs_to_qap(m);
+    init_reductions_uscs_to_ssp(m);
 }
---
+++ src/PyZPK/reductions/uscs_to_ssp.cpp
@@ -0,0 +1,34 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
+#include <libff/algebra/fields/field_utils.hpp>
+#include <libff/algebra/scalar_multiplication/multiexp.hpp>
+#include <libff/common/profiling.hpp>
+#include <libff/common/utils.hpp>
+#include <libsnark/relations/arithmetic_programs/ssp/ssp.hpp>
+#include <libsnark/relations/constraint_satisfaction_problems/uscs/uscs.hpp>
+#include <libsnark/reductions/uscs_to_ssp/uscs_to_ssp.hpp>
+using namespace std;
+namespace py = pybind11;
+using namespace libsnark;
+using namespace libff;
+
+//  Interfaces for a USCS-to-SSP reduction
+void init_reductions_uscs_to_ssp(py::module &m)
+{
+    using FieldT = Fp_model<5l, libff::mnt46_modulus_B>;
+
+    // Instance map for the USCS-to-SSP reduction.
+    m.def("uscs_to_ssp_instance_map", &uscs_to_ssp_instance_map<FieldT>, py::arg("cs"));
+
+    // Instance map for the USCS-to-SSP reduction followed by evaluation of the resulting SSP instance.
+    m.def("uscs_to_ssp_instance_map_with_evaluation", &uscs_to_ssp_instance_map_with_evaluation<FieldT>,
+          py::arg("cs"), py::arg("t"));
+
+    // Witness map for the USCS-to-SSP reduction.
+    // The witness map takes zero knowledge into account when d is random.
+    m.def("uscs_to_ssp_witness_map", &uscs_to_ssp_witness_map<FieldT>,
+          py::arg("cs"), py::arg("primary_input"), py::arg("auxiliary_input"),
+          py::arg("d"));
+}
