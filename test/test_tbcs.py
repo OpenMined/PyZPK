8fd19e576399ad78b7a667256c1fc6992fa94dd4
gargarchit <garg4.ag@gmail.com>
2020-06-25 12:01:20 +0530

    Add test for TBCS

--- test/test_tbcs.py
+++ test/test_tbcs.py
@@ -22,4 +22,59 @@ def test_tbcs_gate_type_enum():
     assert pyzpk.tbcs_gate_type.TBCS_GATE_NAND == 14
     assert pyzpk.tbcs_gate_type.TBCS_GATE_CONSTANT_1 == 15
 
+# Test for TBCS
+# Steps:
+# 1. First we have to select primary and auxiliary inputs uniformly at random
+# 2. For each gate selecting random left and right wires from primary inputs, auxiliary inputs,
+# and outputs of previous gates selecting a gate type at random
+# 3. subject to the constraint "output = 0" if this is an output gate.
 
+
+@pytest.mark.parametrize("primary_input_size, auxiliary_input_size, num_gates, num_outputs",
+                         [(10, 10, 20, 5)])
+def test_tbcs(primary_input_size, auxiliary_input_size, num_gates, num_outputs):
+    assert num_outputs <= num_gates
+    primary_input_size = 10
+    auxiliary_input_size = 10
+    num_gates = 20
+    num_outputs = 5
+    RAND_MAX = 32767
+    num_tbcs_gate_types = 16
+
+    example = pyzpk.tbcs_example()
+
+    primary_input_list = list()
+    auxiliary_input_list = list()
+    for i in range(0, primary_input_size):
+        primary_input_list.append(
+            False if random.randint(0, RAND_MAX) % 2 == 0 else True)
+    for i in range(0, auxiliary_input_size):
+        auxiliary_input_list.append(
+            False if random.randint(0, RAND_MAX) % 2 == 0 else True)
+
+    example.circuit.primary_input_size = primary_input_size
+    example.circuit.auxiliary_input_size = auxiliary_input_size
+
+    all_vals = list()
+    all_vals.extend(primary_input_list)
+    all_vals.extend(auxiliary_input_list)
+    for i in range(0, num_gates):
+        num_variables = primary_input_size + auxiliary_input_size + i
+        gate = pyzpk.tbcs_gate()
+        gate.left_wire = random.randint(0, RAND_MAX) % (num_variables+1)
+        gate.right_wire = random.randint(0, RAND_MAX) % (num_variables+1)
+        gate.output = num_variables+1
+        if(i >= num_gates - num_outputs):
+            while gate.evaluate(all_vals):
+                gate.type = (pyzpk.tbcs_gate_type)(
+                    random.randint(0, RAND_MAX) % num_tbcs_gate_types)
+            gate.is_circuit_output = True
+        else:
+            gate.type = (pyzpk.tbcs_gate_type)(
+                random.randint(0, RAND_MAX) % num_tbcs_gate_types)
+            gate.is_circuit_output = False
+        example.circuit.add_gate(gate)
+        all_vals.append(gate.evaluate(all_vals))
+
+    assert example.circuit.is_satisfied(
+        primary_input_list, auxiliary_input_list)
