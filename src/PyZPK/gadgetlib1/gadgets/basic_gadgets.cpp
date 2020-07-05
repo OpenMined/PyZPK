154238dc7f0a6e8c08ae3c9058b8cb647b0194f3
gargarchit <garg4.ag@gmail.com>
2020-07-05 13:11:19 +0530

    Add bindings for copy gadgets

--- src/PyZPK/gadgetlib1/gadgets/basic_gadgets.cpp
+++ src/PyZPK/gadgetlib1/gadgets/basic_gadgets.cpp
@@ -37,8 +37,39 @@ void declare_multipacking_gadget(py::module &m)
         .def("generate_r1cs_witness_from_bits", &multipacking_gadget<FieldT>::generate_r1cs_witness_from_bits);
 }
 
+void declare_field_vector_copy_gadget(py::module &m)
+{
+    using FieldT = Fp_model<5l, libff::mnt46_modulus_B>;
+
+    py::class_<field_vector_copy_gadget<FieldT>>(m, "field_vector_copy_gadget")
+        .def(py::init<protoboard<FieldT> &,
+                      const pb_variable_array<FieldT> &,
+                      const pb_variable_array<FieldT> &,
+                      const pb_linear_combination<FieldT> &,
+                      const std::string &>())
+        .def("generate_r1cs_constraints", &field_vector_copy_gadget<FieldT>::generate_r1cs_constraints)
+        .def("generate_r1cs_witness", &field_vector_copy_gadget<FieldT>::generate_r1cs_witness);
+}
+
+void declare_bit_vector_copy_gadget(py::module &m)
+{
+    using FieldT = Fp_model<5l, libff::mnt46_modulus_B>;
+
+    py::class_<bit_vector_copy_gadget<FieldT>>(m, "bit_vector_copy_gadget")
+        .def(py::init<protoboard<FieldT> &,
+                      const pb_variable_array<FieldT> &,
+                      const pb_variable_array<FieldT> &,
+                      const pb_linear_combination<FieldT> &,
+                      const size_t,
+                      const std::string &>())
+        .def("generate_r1cs_constraints", &bit_vector_copy_gadget<FieldT>::generate_r1cs_constraints, py::arg("enforce_source_bitness"), py::arg("enforce_target_bitness"))
+        .def("generate_r1cs_witness", &bit_vector_copy_gadget<FieldT>::generate_r1cs_witness);
+}
+
 void init_gadgetlib1_gadgets_basic_gadgets(py::module &m)
 {
     declare_packing_gadget(m);
     declare_multipacking_gadget(m);
+    declare_field_vector_copy_gadget(m);
+    declare_bit_vector_copy_gadget(m);
 }
